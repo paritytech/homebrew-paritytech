5f3c2bb1ed8734c9aee8851c12be2ee8d5358a71
Marek Kotewicz <marek.kotewicz@gmail.com>
2016-02-01 11:53:17 +0100

    Update parity.rb

--- parity.rb
+++ parity.rb
@@ -4,7 +4,7 @@ class Parity < Formula
   version '0.9'
 
   homepage 'https://github.com/ethcore/parity'
-  url 'git@github.com:ethcore/parity.git', :branch => 'master'
+  url 'https://github.com/ethcore/parity', :branch => 'master', :using => :git
 
   depends_on 'multirust' => :build
   depends_on 'rocksdb'
