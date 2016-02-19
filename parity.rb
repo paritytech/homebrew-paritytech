3f06f0d8d4bd57090d055194b11f15177c8a3d03
debris <marek.kotewicz@gmail.com>
2016-02-19 13:52:52 +0100

    stable,beta,master options

--- parity.rb
+++ parity.rb
@@ -4,14 +4,33 @@ class Parity < Formula
   version '0.9.1'
 
   homepage 'https://github.com/ethcore/parity'
-  url 'https://github.com/ethcore/parity.git', :branch => 'beta', :using => :git
+
+  if build.include? "master"
+    url 'https://github.com/ethcore/parity.git', :branch => 'master', :using => :git
+  elsif build.include? "beta"
+    url 'https://github.com/ethcore/parity.git', :branch => 'beta', :using => :git
+  else
+    url 'https://github.com/ethcore/parity.git', :branch => 'beta', :using => :git
+  end
 
   depends_on 'multirust' => :build
   depends_on 'rocksdb'
 
+  option 'master', 'Build and install nightly version.'
+  option 'beta', 'Build and install latest beta.'
+  option 'stable', 'Install latest stable (default).'
+
   def install
-    system "multirust update nightly"
-    system "multirust run nightly cargo build --release --features rpc"
+    if build.include? "master"
+      system "multirust update beta"
+      system "multirust run beta cargo build --release"
+    elsif build.include? "beta"
+      system "multirust update nightly-2016-02-14"
+      system "multirust run nightly-2016-02-14 cargo build --release"
+    else
+      system "multirust update nightly-2016-02-14"
+      system "multirust run nightly-2016-02-14 cargo build --release"
+    end
     bin.install "target/release/parity"
   end
 
