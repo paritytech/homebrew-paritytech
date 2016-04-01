19dbf6777e2bd2bd17fd167a6e0881835f211a6f
Marek Kotewicz <marek.kotewicz@gmail.com>
2016-04-01 09:53:51 +0200

    Update parity.rb

--- parity.rb
+++ parity.rb
@@ -7,10 +7,10 @@ class Parity < Formula
     version '1.1.0'
     url 'https://github.com/ethcore/parity.git', :branch => 'master', :using => :git
   elsif build.include? "beta"
-    version '1.0.0'
+    version '1.0.1'
     url 'https://github.com/ethcore/parity.git', :branch => 'beta', :using => :git
   else
-    version '1.0.0'
+    version '1.0.1'
     url 'https://github.com/ethcore/parity.git', :branch => 'beta', :using => :git
   end
 
