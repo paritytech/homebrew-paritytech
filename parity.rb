5b220e59c2ec97de523071902c447e210bfb3f1a
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2016-11-18 22:51:05 +0100

    Update parity.rb

--- parity.rb
+++ parity.rb
@@ -7,11 +7,11 @@ class Parity < Formula
     version '1.5.0'
     url 'https://github.com/ethcore/parity.git', :branch => 'master', :using => :git
   elsif build.include? "beta"
-    version '1.4.3'
-    url 'https://github.com/ethcore/parity.git', :tag => 'v1.4.3', :using => :git
+    version '1.4.4'
+    url 'https://github.com/ethcore/parity.git', :tag => 'v1.4.4', :using => :git
   else
-    version '1.3.12'
-    url 'https://github.com/ethcore/parity.git', :tag => 'v1.3.12', :using => :git
+    version '1.3.13'
+    url 'https://github.com/ethcore/parity.git', :tag => 'v1.3.13', :using => :git
   end
 
   depends_on 'multirust' => :build
