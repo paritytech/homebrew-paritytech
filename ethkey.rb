4c0545392669b1b6be700d27047ee1c155a98825
debris <marek.kotewicz@gmail.com>
2017-06-18 11:49:25 +0200

    updated repo links and build instructions for ethkey and ethstore

--- ethkey.rb
+++ ethkey.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Ethkey < Formula
-  homepage 'https://github.com/ethcore/parity'
+  homepage 'https://github.com/paritytech/parity'
 
   version '0.2.0'
-  url 'https://github.com/ethcore/parity.git', :branch => 'master', :using => :git
+  url 'https://github.com/paritytech/parity.git', :branch => 'master', :using => :git
 
   depends_on 'multirust' => :build
 
   def install
     system "multirust update stable"
-    system "multirust run stable cargo build --release -p ethkey --features='ethkey-cli'"
+    system "multirust run stable cargo build --release -p ethkey-cli"
     bin.install "target/release/ethkey"
   end
 end
--- ethstore.rb
+++ ethstore.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Ethstore < Formula
-  homepage 'https://github.com/ethcore/parity'
+  homepage 'https://github.com/paritytech/parity'
 
   version '0.1.0'
-  url 'https://github.com/ethcore/parity.git', :branch => 'master', :using => :git
+  url 'https://github.com/paritytech/parity.git', :branch => 'master', :using => :git
 
   depends_on 'multirust' => :build
 
   def install
     system "multirust update stable"
-    system "multirust run stable cargo build --release -p ethstore --features='ethstore-cli'"
+    system "multirust run stable cargo build --release -p ethstore-cli"
     bin.install "target/release/ethstore"
   end
 end
