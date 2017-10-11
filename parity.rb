3902fe6ee77b7a499d484f678e9d5c72011c7e4e
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2017-10-11 23:48:24 +0200

    Update parity.rb

--- parity.rb
+++ parity.rb
@@ -11,9 +11,9 @@ class Parity < Formula
     url 'http://d1h4xl4cr1h0mo.cloudfront.net/v1.6.10/x86_64-apple-darwin/parity'
     sha256 "a65c24d9d1746b4c12c32db36e01839e903ec712731670ad856c6046675ce49c"
   else
-    version '1.7.3'
+    version '1.7.4'
     url 'http://d1h4xl4cr1h0mo.cloudfront.net/v1.7.3/x86_64-apple-darwin/parity'
-    sha256 "bb72cfde56f11a69a2d7f96f5eefffc6214c12a5895721ed8ad518d639e7e8bf"
+    sha256 "d06e3a6f333bb3e41d8d31793ea72c7bd22dc73ee79a112c16604b479d9ea21a"
   end
 
   option 'master', 'Install nightly version.'
