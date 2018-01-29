6b9acfac269ce0925bfec155082095db9e8186b0
André Silva <andre.beat@gmail.com>
2018-01-29 10:29:14 +0000

    fix <<-EOS.undent deprecation (#67)

--- parity.rb
+++ parity.rb
@@ -36,7 +36,7 @@ class Parity < Formula
     system "#{bin}/delta", "--version"
   end
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
