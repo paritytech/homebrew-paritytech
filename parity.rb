356d662ee1d6dceb8582c1d33f7c049301600e54
Afri Schoedon <5chdn@users.noreply.github.com>
2019-02-13 15:39:31 +0100

    Update parity.rb

--- parity.rb
+++ parity.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Parity < Formula
   homepage 'https://github.com/paritytech/parity'
-  version '2.2.9'
-  url 'https://releases.parity.io/ethereum/v2.2.9/x86_64-apple-darwin/parity'
-  sha256 '1690ea47227e45b3b67256734e3020a958c9a4b6c458fa21bbda2f55d91819ed'
+  version '2.2.10'
+  url 'https://releases.parity.io/ethereum/v2.2.10/x86_64-apple-darwin/parity'
+  sha256 '6267f80dcf6709e40074b8bf369999be0615bbc075c281cbe483cfe0095801e7'
 
   devel do
-    version '2.3.2'
-    url 'https://releases.parity.io/ethereum/v2.3.2/x86_64-apple-darwin/parity'
-    sha256 '66afa1e9c8d6c611ff5fb20a2c62a6c2de24cd9baf5f209b6a0a97ed1aac65d3'
+    version '2.3.3'
+    url 'https://releases.parity.io/ethereum/v2.3.3/x86_64-apple-darwin/parity'
+    sha256 '83edca7466905a305c88303cc6f96a4cec495c194df557dd6046243a10d9a483'
   end
 
   head do
