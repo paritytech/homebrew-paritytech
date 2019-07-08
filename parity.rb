52c89233b49301f908533bd316829944a29b2358
Martin Pugh <pugh@s3kr.it>
2019-07-08 16:59:43 +0200

    update parity.rb

--- parity.rb
+++ parity.rb
@@ -2,18 +2,18 @@ require 'formula'
 
 class Parity < Formula
   homepage 'https://github.com/paritytech/parity'
-  version '2.4.9'
+  version '2.5.5'
   url 'https://releases.parity.io/ethereum/v2.4.8/x86_64-apple-darwin/parity'
   sha256 '69f1ccf4231d66949e57e5713e2adf405d29a3b42561a6c1251846f3fced03d6'
 
   devel do
-    version '2.5.4'
+    version '2.6.0'
     url 'https://releases.parity.io/ethereum/v2.5.3/x86_64-apple-darwin/parity'
     sha256 '136284a143ec6665f7c9ebb7d81a913120e7ec1177b0c1b29d1d2c458df42e17'
   end
 
   head do
-    version '2.6.0'
+    version '2.7.0'
     url 'https://releases.parity.io/ethereum/nightly/x86_64-apple-darwin/parity'
   end
 
